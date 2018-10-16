f250f158af7cdaa415a9289990ff61ef415ac431
Syltech <techene.sylvain@gmail.com>
2018-10-16 16:34:10 +0200

    Ajout de la fonctionnallité de recherche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {TmdbService} from './tmdb.service';
@@ -8,17 +9,24 @@ import {AngularFireModule} from '@angular/fire';
 import {environment} from '../environments/environment';
 import {AngularFireAuthModule} from '@angular/fire/auth';
 import {AngularFireDatabaseModule} from '@angular/fire/database';
+import { FilmComponent } from './film/film.component';
+import { ListeFilmsComponent } from './liste-films/liste-films.component';
+import { RechercheComponent } from './recherche/recherche.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmComponent,
+    ListeFilmsComponent,
+    RechercheComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AngularFireModule.initializeApp( environment.firebase ),
     AngularFireAuthModule,
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    FormsModule
   ],
   providers: [TmdbService],
   bootstrap: [AppComponent]
