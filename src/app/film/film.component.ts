c9b44ce639763716029758d65a11fbbd3592e551
Pauline Allemand <allemanp@f203-10.e-im2ag.ujf-grenoble.fr>
2018-10-17 16:04:12 +0200

    correction composant film, reste a retrouver les acteurs d'un film et tester avec une ID

--- src/app/film/film.component.ts
+++ src/app/film/film.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {TmdbService} from "../tmdb.service";
+import {MovieGenre, MovieResponse, ProductionCompany, ProductionCountry, SpokenLanguage} from "../tmdb-data/Movie";
 
 @Component({
   selector: 'app-film',
@@ -9,30 +10,37 @@ import {TmdbService} from "../tmdb.service";
 })
 export class FilmComponent implements OnInit {
   movieId: number;
-  annee: number;
-  genre: string;
+  title: string;
+  annee: string;
+  genre: MovieGenre[];
   status: string;
-  compagnie: string;
-  pays: string;
-  langue: string;
-  revenue: string;
-  bugdet: string;
+  compagnie: ProductionCompany[];
+  pays: ProductionCountry[];
+  langue: SpokenLanguage[];
+  revenue: number;
+  bugdet: number;
   synopsis: string;
-  /*acteurs: string[]*/
+  poster_path: string;
+  film: MovieResponse;
+  //acteurs: ;
 
 
   constructor(private route: ActivatedRoute, private tmdb: TmdbService) {
-    this.movieId = 0;
-    annee: number;
-    genre: string;
-    status: string;
-    compagnie: string;
-    pays: string;
-    langue: string;
-    revenue: string;
-    bugdet: string;
-    synopsis: string;
-    this.tmdb.getMovie(this.movieId);
+    this.movieId = 335983;
+    this.tmdb.getMovie(this.movieId).then(res =>{
+      this.title = res.title;
+      this.annee = res.release_date;
+      this.status     = res.status;
+      this.revenue    = res.revenue;
+      this.bugdet     = res.budget;
+      this.synopsis   = res.overview;
+      this.genre      = res.genres;
+      this.compagnie  = res.production_companies;
+      this.pays       = res.production_countries;
+      this.langue     = res.spoken_languages;
+      this.poster_path = res.poster_path;
+      //this.acteurs  = res. ?????? ;
+    });
   }
 
   ngOnInit() {
