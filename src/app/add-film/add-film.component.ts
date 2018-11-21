8abde340b5180796834e95c568715d238f3ed073
Pauline Allemand <allemanp@F211-10.e-im2ag.ujf-grenoble.fr>
2018-11-21 15:01:02 +0100

    ajout champ texte input pour add un film a une liste

--- src/app/add-film/add-film.component.ts
+++ src/app/add-film/add-film.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ListStructure} from "../dataTypes/ListStructure";
+import {ActivatedRoute} from "@angular/router";
+import {TmdbService} from "../tmdb.service";
+import {MovieListService} from "../movie-list.service";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-add-film',
@@ -7,9 +12,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddFilmComponent implements OnInit {
 
-  constructor() { }
+  @Input() movieId: number;
+
+  lists: ListStructure[];
+  isInAList: boolean;
+  isUserLogged: boolean;
+  newList: string;
+
+  constructor(private route: ActivatedRoute, private tmdb: TmdbService, private movieList: MovieListService) {
+    this.isUserLogged = false;
+    this.movieList.getUser().subscribe(u => {
+
+      this.isUserLogged = false;
+
+      if (u) { // User is logged
+        this.isUserLogged = true;
+        this.movieList.getUserLists().snapshotChanges().pipe(
+          map( changes => {
+            return changes.map(c => ({key: c.payload.key, ...c.payload.val()}));
+          })
+        ).subscribe(lists => {
+          this.lists = lists;
+          this.isInAList = false;
+          this.lists.forEach(l => this.isInTheList(l));
+        });
+      }
+    });
+  }
 
   ngOnInit() {
+  }
+
+  addToList(list: ListStructure) {
+    this.movieList.addMovie(list, this.movieId);
+  }
+
+  isInTheList(list: ListStructure): boolean {
+    if (list.movies && list.movies.filter(m => m === this.movieId).length > 0) {
+      this.isInAList = true;
+      return true;
+    }
+    return false;
+  }
+
+  createList() {
+    const l: ListStructure = new ListStructure();
+    l.name = this.newList;
+    this.movieList.createList(l);
+    this.newList = "";
   }
 
 }
